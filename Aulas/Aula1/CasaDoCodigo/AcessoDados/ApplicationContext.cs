5043dfb1503e26cbc1c88e840cec9f6f2135a488
Willian <Willian@DESKTOP-B1T9NO5>
2019-02-12 16:04:56 -0200

    Adicionar arquivos de projeto.

---
+++ Aulas/Aula1/CasaDoCodigo/AcessoDados/ApplicationContext.cs
@@ -0,0 +1,49 @@
+﻿using CasaDoCodigo.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CasaDoCodigo.AcessoDados
+{
+    public class ApplicationContext : DbContext
+    {
+        //Opcional se declarada as criações da tabelas no modelbuilder
+        public DbSet<Cadastro> Cadastro { get; set; }
+
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Add propriedade ToTable se der erro de FK
+            modelBuilder.Entity<Produto>().HasKey(p => p.Id);
+            modelBuilder.Entity<Produto>().ToTable("Produto");
+
+            modelBuilder.Entity<Pedido>().HasKey(p => p.Id);
+            modelBuilder.Entity<Pedido>().HasMany(p => p.Itens).WithOne(p => p.Pedido);
+            modelBuilder.Entity<Pedido>().HasOne(p => p.Cadastro).WithOne(p => p.Pedido).IsRequired();
+            //modelBuilder.Entity<Pedido>().OwnsOne(p => p.Cadastro);
+            modelBuilder.Entity<Pedido>().ToTable("Pedido");
+
+
+            modelBuilder.Entity<ItemPedido>().HasKey(p => p.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(p => p.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(p => p.Produto);
+            modelBuilder.Entity<ItemPedido>().ToTable("ItemPedido");
+
+            modelBuilder.Entity<Cadastro>().HasKey(p => p.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(p => p.Pedido);
+            modelBuilder.Entity<Cadastro>().ToTable("Cadastro");
+
+
+
+        }
+    }
+}
---
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasaDoCodigo.AcessoDados;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc();
+
+            //Fornece a StringConnetions ao DbContext
+            services.AddDbContext<ApplicationContext>(options => options
+                .UseSqlServer("Server=localhost;Database=CasaDoCodigo;Trusted_Connection=True;MultipleActiveResultSets=true"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                //app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
+            });
+            //adicionar IServiceProvider como parametro no metodo Configure
+            //Adiciona a verificação do entityframework em toda inicialização para verificar se existe as tabelas no BD, se naõ existir ele cria autamaticamente.
+            serviceProvider.GetService<ApplicationContext>().Database.EnsureCreated();
+        }
+    }
+}
